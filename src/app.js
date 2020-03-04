dac1ca3c1cc2c47ebe5036803576746b35f063db
Забозлаев Илья Вячеславович <zabozlaeviv@sovcombank.ru>
2020-03-04 23:45:15 +0300

    [add] /docs - swagger ui

--- src/app.js
+++ src/app.js
@@ -3,10 +3,15 @@ import { json } from 'body-parser';
 import errorHandler from './api/middleware/error';
 import notFoundHandler from './api/middleware/notFound';
 
+import swaggerUi from 'swagger-ui-express';
+import jsonDoc from '../swagger.json';
+
 import apiRouter from './api';
 
 const app = express()
   .use(json())
+  .use('/docs', swaggerUi.serve)
+  .get('/docs', swaggerUi.setup(jsonDoc))
   .use('/forms', apiRouter)
   .use(notFoundHandler)
   .use(errorHandler);
