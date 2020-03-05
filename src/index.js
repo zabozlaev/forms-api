a646d2a3f90df31e065667eb4272d35582a2318c
Забозлаев Илья Вячеславович <zabozlaeviv@sovcombank.ru>
2020-03-05 10:27:16 +0300

    [add] logging the start

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import { createServer } from 'http';
 
 import app from './app';
 import config from './config';
+import logger from './api/utils/logger';
 
-createServer(app).listen(config.port);
+createServer(app).listen(config.port, () =>
+  logger.info(`Application is ready on ${config.port}`),
+);
